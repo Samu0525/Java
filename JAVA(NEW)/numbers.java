17a7fb37a08d56e006e956b9355c682e3949dc38
Samruddhi Shewale <shewalesamruddhi327@gmail.com>
2026-03-19 20:47:25 +0530

    day12:Armstrong of number

--- JAVA(NEW)/numbers.java
+++ JAVA(NEW)/numbers.java
@@ -1,5 +1,6 @@
 import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
+import java.math.*;
 
 public class numbers {
     public static int reverse(int n){
@@ -22,12 +23,25 @@ public class numbers {
         }
        return sumn;
     }
+    public static int ArmStrong(int n){
+        String str=Integer.toString(n);
+        int l=str.length();
+        int sum=0;
+        while(n!=0){
+            int digit=n%10;
+            //power*=digit;
+             double s=Math.pow(digit,l);// as math.pw return double 
+             sum+=s;
+            n=n/10;
+        }
+        return sum;
+    }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a number:");
         int n=sc.nextInt();
         while(true){
-            System.out.println("1.reverse 2.Exit 3.sum");
+            System.out.println("1.reverse 2.Exit 3.sum 4.Armstong value");
             int choice=sc.nextInt();
             switch (choice) {
                 case 1:
@@ -38,6 +52,9 @@ public class numbers {
                     return;
                 case 3:
                     System.out.println("Sum:"+sumn(n));
+                    break;
+                case 4:
+                    System.out.println("Armstrong of Number:"+ArmStrong(n));
                     break;
                 default:
                     break;
